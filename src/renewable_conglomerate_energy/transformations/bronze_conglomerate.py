48b0b3dcbfd4c730a6f24a9bf72cf864ff8d6568
Andres Felipe Lagos Ruiz <68169743+oxiboy@users.noreply.github.com>
2026-05-22 11:26:43 -0500

    Ree 6 (#9)
    * checkpoint
    * ree_6 finish first join
    ---------
    
    Co-authored-by: Andres Lagos <andres.lagos@evalueserve.com>

---
+++ src/renewable_conglomerate_energy/transformations/bronze_conglomerate.py
@@ -0,0 +1,121 @@
+# Databricks notebook source
+# COMMAND ----------
+import dlt
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+env = schema = str()
+env = spark.conf.get("catalog")
+schema = spark.conf.get("schema")
+
+raw_path = {"hydropower_consumption": f"/Volumes/{env}/{schema}/lookup/hydropower_consumption/",
+            "installed_solar": f"/Volumes/{env}/{schema}/lookup/installed_solar/",
+            "renewable_energy_consumption": f"/Volumes/{env}/{schema}/lookup/renewable_energy_consumption/",
+            "modern_renewable_prod": f"/Volumes/{env}/{schema}/lookup/modern_renewable_prod/",
+            "share_electricity_renewable": f"/Volumes/{env}/{schema}/lookup/share_electricity_renewable/"}
+
+##TODO add liquid cluster
+# COMMAND ----------
+@dlt.table(
+    table_properties={"quality": "bronze"}
+)
+def bronze_hydropower_consumption():
+
+    return (
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.format", "csv")
+        .option("cloudFiles.inferColumnTypes", "true")
+        .option("cloudFiles.schemaEvolutionMode", "addNewColumnsWithTypeWidening")
+        .option("header", "true")
+        .option("sep", ",")
+        .load(raw_path['hydropower_consumption'])
+        .selectExpr(
+            "*",
+            "_metadata.file_modification_time AS modification_date",
+            "_metadata.file_path as file_path",
+        )
+    )
+
+# COMMAND ----------
+@dlt.table(
+    table_properties={"quality": "bronze"}
+)
+def bronze_installed_solar_PV_capacity():
+
+    return (
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.format", "csv")
+        .option("cloudFiles.inferColumnTypes", "true")
+        .option("cloudFiles.schemaEvolutionMode", "addNewColumnsWithTypeWidening")
+        .option("header", "true")
+        .option("sep", ",")
+        .load(raw_path['installed_solar'])
+        .selectExpr(
+            "*",
+            "_metadata.file_modification_time AS modification_date",
+            "_metadata.file_path as file_path",
+        )
+    )
+
+# COMMAND ----------
+@dlt.table(
+    table_properties={"quality": "bronze"}
+)
+def bronze_modern_renewable_energy_consumption():
+
+    return (
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.format", "csv")
+        .option("cloudFiles.inferColumnTypes", "true")
+        .option("cloudFiles.schemaEvolutionMode", "addNewColumnsWithTypeWidening")
+        .option("header", "true")
+        .option("sep", ",")
+        .load(raw_path['renewable_energy_consumption'])
+        .selectExpr(
+            "*",
+            "_metadata.file_modification_time AS modification_date",
+            "_metadata.file_path as file_path",
+        )
+    )
+
+# COMMAND ----------
+@dlt.table(
+    table_properties={"quality": "bronze"}
+)
+def bronze_modern_renewable_prod():
+
+    return (
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.format", "csv")
+        .option("cloudFiles.inferColumnTypes", "true")
+        .option("cloudFiles.schemaEvolutionMode", "addNewColumnsWithTypeWidening")
+        .option("header", "true")
+        .option("sep", ",")
+        .load(raw_path['modern_renewable_prod'])
+        .selectExpr(
+            "*",
+            "_metadata.file_modification_time AS modification_date",
+            "_metadata.file_path as file_path",
+        )
+    )
+
+# COMMAND ----------
+@dlt.table(
+    table_properties={"quality": "bronze"}
+)
+def bronze_share_electricity_renewable():
+
+    return (
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.format", "csv")
+        .option("cloudFiles.inferColumnTypes", "true")
+        .option("cloudFiles.schemaEvolutionMode", "addNewColumnsWithTypeWidening")
+        .option("header", "true")
+        .option("sep", ",")
+        .load(raw_path['share_electricity_renewable'])
+        .selectExpr(
+            "*",
+            "_metadata.file_modification_time AS modification_date",
+            "_metadata.file_path as file_path",
+        )
+    )
---
+++ src/renewable_conglomerate_energy/transformations/silver_conglomerate.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+# COMMAND ----------
+from pyspark import pipelines as dp
+import pyspark.sql.functions as F
+
+
+#TODO check column key format, expecific columns, add alias
+@dp.table(name = "silver_conglomerate_energy.silver_fact_energy_consumption",
+          table_properties = {
+                            "quality":"silver"
+                        })
+def silver_fact_energy_consumption():
+    return (
+    dp.read_stream("bronze_modern_renewable_energy_consumption") \
+    .join(dp.read_stream("bronze_hydropower_consumption"), on=["Entity", "Year"], how='inner')) \
+    .select("bronze_modern_renewable_energy_consumption.Entity",
+            "bronze_modern_renewable_energy_consumption.Year",
+            "bronze_modern_renewable_energy_consumption.Geo_Biomass_Other",          
+            "bronze_modern_renewable_energy_consumption.Solar_Generation",
+            "bronze_modern_renewable_energy_consumption.Wind_Generation",
+            "bronze_modern_renewable_energy_consumption.Hydro_Generation",
+            "bronze_hydropower_consumption.Electricity_from_hydro"
+            )
